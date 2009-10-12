edb6d81e9aebb24ac573f1c60a77e58d027c05d7
Martin Morgan <mtmorgan@fhcrc.org>
2009-10-12 23:54:11 +0000

    Use unsigned long for bit64_t if it is 8 bytes, otherwise unsigned long long (which will fail with g++ -std=g++98 -pedantic or ISO C++ compilers)
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ShortRead@42336 bc3139a8-67e5-0310-9ffc-ced21a209358

--- src/const.h
+++ src/const.h
@@ -1,13 +1,24 @@
-/* Note: This file has been copied at verbatim (except for the
-present note) from the source code of Maq, version 0.6.5, and is
-hence copyright (c) Li Hang, who has released Maq under GPL 2 */
+/* 
+
+   Note: This file has been copied from the source code of Maq,
+   version 0.6.5, and is hence copyright (c) Li Hang, who has released
+   Maq under GPL 2.
+
+   The test for SIZEOF_UNSIGNED_LONG == 8 has been added, and depends
+   on ../configure.ac
+
+*/
 
 #ifndef NST_CONST_H
 #define NST_CONST_H
 
 #define MAX_ULL 0xffffffffffffffffull
 
+#if SIZEOF_UNSIGNED_LONG == 8
+typedef unsigned long bit64_t;
+#else
 typedef unsigned long long bit64_t;
+#endif
 typedef unsigned bit32_t;
 typedef unsigned short bit16_t;
 typedef unsigned char bit8_t;
