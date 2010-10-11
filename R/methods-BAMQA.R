3250469def377c9a5aa53b065e4e39b5c30615b4
Valerie Obenchain <vobencha@fhcrc.org>
2010-10-11 19:25:14 +0000

    formatting and function clean up
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/ShortRead@50105 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-BAMQA.R
+++ R/methods-BAMQA.R
@@ -10,51 +10,44 @@
     if (verbose)
         message("qa 'BAM' pattern:", pattern)
     rpt <- readAligned(dirPath, pattern, type, ...)
-	doc <- .qa_depthOfCoverage(rpt, pattern)
-    # adapter contamination is computed in qa() 
+    doc <- .qa_depthOfCoverage(rpt, pattern)
+    # adapter contamination is computed in qa()
     res <-  qa(rpt, pattern, ..., verbose=verbose)
-    c(.srlist(res), 
-		list(depthOfCoverage=doc)) 
+    c(.srlist(res), list(depthOfCoverage=doc))
 }
 
 
 .qa_BAM <-
-    function(dirPath, pattern, type="BAM",..., 
-			 param=list(ScanBamParam(
-               	simpleCigar=TRUE,
-               	reverseComplement=TRUE,
-               	what=.readAligned_bamWhat())),
-			 verbose=FALSE)
-	
+    function(dirPath, pattern, type="BAM",...,
+    param=list(ScanBamParam(
+               simpleCigar=TRUE,
+               reverseComplement=TRUE,
+               what=.readAligned_bamWhat())),
+    verbose=FALSE)
 {
     fls <- .file_names(dirPath, pattern)
-	if(!missing(param)) {
- 		## make param into a list
+    if(!missing(param)) {
+    ## make param into a list
         if (is.list(param) && !all(sapply(param, is, "ScanBamParam"))) {
-            msg <-  paste("All elements of the param",
-                          "argument must be ScanBamParm objects.",
-                          collapse="")
+            msg <-  "all elements of 'param' must be SCanBamParam objects"
             .throw(SRError("UserArgumentMismatch",msg))
         }
         else if (!is.list(param) && !is(param, "ScanBamParam")) {
-            msg <-  paste("The param argument must be a",
-                          "ScanBamParm object.",
-                          collapse="")
+            msg <-  "'param' must be a ScanBamParam object"
             .throw(SRError("UserArgumentMismatch",msg))
-		}
+        }
         else if (!is.list(param) && is(param, "ScanBamParam")) {
             param <- list(param)
-		}
+        }
 
         if (length(param) > 1L && (length(param) != length(fls))) {
-            msg <-  paste("The length of the param argument does not",
-                          "match the number of BAM files to be read.",
-                          " If more than one param is supplied there must",
-                          "be one for each file.", collapse="")
+            msg <-  paste("length(param) != length(files);",
+                          "if more than one param is supplied there must",
+                          "be one for each file")
             .throw(SRError("UserArgumentMismatch",msg))
         }
 
- 		## FIXME: currently we only deal with cigars without indels
+        ## FIXME: currently we only deal with cigars without indels
         if (any(sapply(param, bamSimpleCigar) != TRUE)) {
             msg <- paste("using 'TRUE' for 'bamSimpleCigar(param)'",
                 "(skipping reads with I, D, H, S, or P in 'cigar')")
@@ -74,20 +67,16 @@
             .throw(SRWarn("UserArgumentMismatch", msg))
         }
 
-     	param <- sapply(param,
-                    	FUN = function(param) {
-                    	initialize(param, simpleCigar=TRUE,
-                    	reverseComplement=TRUE,
-                    	what=.readAligned_bamWhat())})
+        param <-
+            lapply(param, initialize, simpleCigar=TRUE,
+                   reverseComplement=TRUE,
+                   what=.readAligned_bamWhat())
+    }
 
-	}
-        DF <- DataFrame(file=fls)
-        DF$param <- param
-
-	lst <- mapply(.qa_BAM_lane, pattern=basename(DF$file), 
-				  param=DF$param, MoreArgs=list(dirPath=dirPath, ...), 
-			      SIMPLIFY=FALSE, USE.NAMES=FALSE, 
-				  verbose=verbose)
+    lst <- mapply(.qa_BAM_lane, pattern=basename(fls),
+        param=param, MoreArgs=list(dirPath=dirPath, ...),
+        SIMPLIFY=FALSE, USE.NAMES=FALSE,
+        verbose=verbose)
 
     lst <-
         list(readCounts=.bind(lst, "readCounts"),
@@ -110,13 +99,13 @@
              }),
              depthOfCoverage=.bind(lst, "depthOfCoverage"),
              adapterContamination=.bind(lst, "adapterContamination")
-		)
+    )
     .BAMQA(lst)
 }
 
 
 
-.report_html_BAMQA <-          
+.report_html_BAMQA <-
     function(x, dest, type, ...)
 {
     qa <- x                             # mnemonic alias
@@ -124,7 +113,7 @@
     fls <- c("0000-Header.html", "1000-Overview.html",
              "2000-RunSummary.html", "3000-ReadDistribution.html",
              "4000-CycleSpecific.html", "8000-DepthOfCoverage.html",
-			 "9000-AdapterContamination.html", "9999-Footer.html")
+             "9000-AdapterContamination.html", "9999-Footer.html")
     sections <- system.file("template", fls, package="ShortRead")
     perCycle <- qa[["perCycle"]]
     values <-
@@ -152,9 +141,9 @@
              DEPTH_OF_COVERAGE_FIGURE=.html_img(
                dest, "depthOfCoverage",
                .plotDepthOfCoverage(qa[["depthOfCoverage"]])),
-			 ADAPTER_CONTAMINATION=hwrite(
-			   .ppnCount(qa[["adapterContamination"]]),
-			   border=NULL)
+             ADAPTER_CONTAMINATION=hwrite(
+             .ppnCount(qa[["adapterContamination"]]),
+             border=NULL)
              )
     .report_html_do(dest, sections, values, ...)
 }
